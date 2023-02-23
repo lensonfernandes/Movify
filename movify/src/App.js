038d52f15013dd6b1855502ae05dbdd748390e32
Lenson Merwin Fernandes <lenson.fernand@gmail.com>
2023-02-23 09:30:28 +0530

    Created Login Component

---
+++ movify/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Login from './components/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />}></Route>
+        </Routes>
+      
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ movify/src/components/Login.jsx
@@ -0,0 +1,118 @@
+import React from "react";
+import styled from "styled-components";
+
+const Login = () => {
+  return (
+    <Container>
+      <Content>
+        <CTA>
+            <CTALogoOne src="/images/cta-logo-one.svg" alt="" /> 
+            <SignUp> Get All Content</SignUp>
+            <Description>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus eos enim adipisci assumenda, odio ducimus inventore totam ipsa quis necessitatibus sit expedita doloribus quaerat delectus.</Description>
+        </CTA>
+        <BgImage />
+      </Content>
+    </Container>
+    //  <div>Login</div>
+  );
+};
+
+const Container = styled.section`
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  height: 100vh;
+`;
+
+const Content = styled.div`
+  margin-bottom: 10vh;
+  width: 100%;
+  position: relative;
+  min-height: 100vh;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 80px 40px;
+  height: 100%;
+`;
+
+const BgImage = styled.div`
+  height: 100%;
+  background-size: cover;
+  background-image: url("/images/login-background.jpg");
+
+  background-position: top;
+  background-repeat: no-repeat;
+
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: -15;
+`;
+
+
+const CTA = styled.div`
+
+margin-bottom: 2vw;
+max-width: 650px;
+flex-wrap: wrap;
+display: flex;
+flex-direction: column;
+justify-content: center;
+margin-top: 0;
+align-items:center;
+text-align:center;
+margin-left: auto;
+margin-right: auto;
+transition-tiiming-function: ease-out;
+transition-opacity: 0.2s;
+width:100%;
+
+
+`;
+
+const CTALogoOne = styled.img`
+
+margin-bottom: 12px;
+max-width: 600px;
+min-height: 1px;
+display: block;
+width: 100%;
+z-index: 5;
+
+`;
+
+
+const SignUp = styled.a`
+
+    font-weight: bold;
+    color: #f9f9f9;
+    background-color: #0063e5;
+    margin-bottom: 12px;
+    width: 100%;
+    letter-spacing: 1.5px;
+    padding: 16.5px  0;
+    border: 1px solid transparent;
+    border-radius: 4px;
+
+
+
+    &:hover {
+        background-color: #0483ee
+    }
+`
+
+
+const Description = styled.h2`
+    color: hsla(0, 0%, 95%, 1);
+    font-size: 11px;
+    margin: 0 0 24px;
+    line-height: 1.5em;
+
+`
+
+export default Login;
