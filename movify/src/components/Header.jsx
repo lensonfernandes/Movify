9dd8f3914d1027ac748bdfbc0befee8647582b2e
Lenson Merwin Fernandes <lenson.fernand@gmail.com>
2023-02-23 12:17:06 +0530

    Login Button Added

--- movify/src/components/Header.jsx
+++ movify/src/components/Header.jsx
@@ -7,7 +7,34 @@ const Header = () => {
         <Logo >
             <img src="/images/logo.svg" alt="" />
         </Logo>
-        <NavMenu> Menu</NavMenu>
+        <NavMenu> 
+            <a href="/home">
+                <img src="images/home-icon.svg" alt="home" />  
+                <span>Home</span>
+            </a>
+            <a >
+                <img src="images/search-icon.svg" alt="search" />  
+                <span>Search</span>
+            </a>
+            <a >
+                <img src="images/watchlist-icon.svg" alt="Watchlist" />  
+                <span>Watchlist</span>
+            </a>
+            <a >
+                <img src="images/original-icon.svg" alt="original" />  
+                <span>Originals</span>
+            </a>
+            <a >
+                <img src="images/movie-icon.svg" alt="movie" />  
+                <span>Movie</span>
+            </a>
+            <a >
+                <img src="images/series-icon.svg" alt="series" />  
+                <span>Series</span>
+            </a>
+          
+        </NavMenu>
+        <Login>Login</Login>
     </Nav>
   )
 }
@@ -54,9 +81,59 @@ position: relative;
 margin-right: auto;
 margin-left: 25px;
 
+
+a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    transition: all 0.2s ease 0s;
+
+    img{
+        height: 20px;
+        min-width: 20px;
+        width: 20px;
+        z-index: auto;
+        
+
+    }
+    span{
+        color: rgb(249, 249, 249);
+        font-size: 13px;
+        letter-spacing: 1.42px;
+        line-height: 1.10px;
+        padding: 2px 0px;
+        white-space: nowrap;
+        position: relative;
+     
+    
+    }
+
+ 
+
+
+}
+
 @media  (max-width: 768px) {
     display: none;
 }
+
+
+`
+
+
+const Login = styled.a`
+    background-color: rgba(0, 0, 0, 0.6);
+    padding: 8px 16px;
+    text-transform: uppercase;
+    letter-spacing: 1.5px;
+    border: 1px solid #f9f9f9;
+    border-radius: 4px;
+    transition: all 0.2s ease 0s;
+
+    &:hover{
+        background-color: #f9f9f9;
+        color:#000;
+    }
 
 `
 
